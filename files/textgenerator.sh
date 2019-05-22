4372fed45e40ef8522cff38648993e2488c5d31f
ccomai92 <Kris@D-10-156-21-170.dhcp4.washington.edu>
2019-05-22 14:36:27 -0700

    done

--- files/textgenerator.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-base64 /dev/urandom | head -c 100000000 > text1.txt
-base64 /dev/urandom | head -c 100000000 > text2.txt
-base64 /dev/urandom | head -c 100000000 > text3.txt
--- files/wordcount.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# $1 word
-# $2 file name
-grep -o $1 $2
